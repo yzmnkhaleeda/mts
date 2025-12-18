167eb09a69e90601de8f7be5a907e258ba38eb0e
yzmnkhaleeda <yzmnnkhaleeda@gmail.com>
2025-12-18 16:47:53 +0800

    Moved controller file into Http

--- routes.php
+++ routes.php
@@ -1,21 +1,21 @@
 <?php
 
-$router->get('/', 'controllers/sessions/create.php')->only('guest');
-$router->post('/', 'controllers/sessions/store.php')->only('guest');
-$router->delete('/sessions', 'controllers/sessions/destroy.php')->only('auth');
+$router->get('/', 'sessions/create.php')->only('guest');
+$router->post('/', 'sessions/store.php')->only('guest');
+$router->delete('/sessions', 'sessions/destroy.php')->only('auth');
 
-$router->get('/dashboard', 'controllers/index.php')->only('auth');
+$router->get('/dashboard', 'index.php')->only('auth');
 
-$router->get('/suppliers', 'controllers/suppliers/index.php')->only('auth');
+$router->get('/suppliers', 'suppliers/index.php')->only('auth');
 
-$router->get('/supplier', 'controllers/suppliers/show.php')->only('auth');
-$router->delete('/supplier', 'controllers/suppliers/destroy.php')->only('auth');
+$router->get('/supplier', 'suppliers/show.php')->only('auth');
+$router->delete('/supplier', 'suppliers/destroy.php')->only('auth');
 
-$router->get('/suppliers/edit', 'controllers/suppliers/edit.php')->only('auth');
-$router->patch('/suppliers/edit', 'controllers/suppliers/update.php')->only('auth');
+$router->get('/suppliers/edit', 'suppliers/edit.php')->only('auth');
+$router->patch('/suppliers/edit', 'suppliers/update.php')->only('auth');
 
-$router->get('/suppliers/create', 'controllers/suppliers/create.php')->only('auth');
-$router->post('/suppliers/create', 'controllers/suppliers/store.php')->only('auth');
+$router->get('/suppliers/create', 'suppliers/create.php')->only('auth');
+$router->post('/suppliers/create', 'suppliers/store.php')->only('auth');
 
-$router->get('/admin/users/create', 'controllers/users/create.php')->only('admin');
-$router->post('/admin/users', 'controllers/users/store.php')->only('admin');
+$router->get('/admin/users/create', 'users/create.php')->only('admin');
+$router->post('/admin/users', 'users/store.php')->only('admin');
